d02085b5302ad61346ddae0913b01478c83e2df3
Benja Supra <benjamin.ojeda@mail.udp.cl>
2022-05-22 16:34:07 -0400

    producer and consumer comunication

--- backend/index.js
+++ backend/index.js
@@ -9,15 +9,30 @@ app.use(cors());
 app.use(express.json());
 
 const kafka = new Kafka({
-  brokers: [process.env.kafkaHost]
+  brokers: ['kafka:9092']
 });
 
-const producer = kafka.producer();
+const producer = kafka.producer()
+
 
 app.get("/", async (req, res) => {
     res.send("api login");
+
+    await producer.send({
+      topic: 'test-topic',
+      messages: [
+        { value: 'Hello KafkaJS user!' },
+      ],
+    })
+
 });
 
-app.listen(port, () => {
+app.post("/login", async(req,res) =>{
+  res.send("true or false")
+});
+
+app.listen(port, async() => {
   console.log(`API RUN AT http://localhost:${port}`);
+  await producer.connect()
+
 });
--- bloqueo/index.js
+++ bloqueo/index.js
@@ -9,15 +9,31 @@ app.use(cors());
 app.use(express.json());
 
 const kafka = new Kafka({
-  brokers: [process.env.kafkaHost]
+  brokers: ['kafka:9092']
 });
 
-const producer = kafka.producer();
+const consumer = kafka.consumer({ groupId: 'test-group' })
 
 app.get("/", async (req, res) => {
     res.send("baneados");
 });
 
-app.listen(port, () => {
+app.get("/blocked", async (req, res) =>{
+    res.send("usuarios bloqueados");
+})
+
+app.listen(port, async() => {
   console.log(`API RUN AT http://localhost:${port}`);
+
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+  
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        value: message.value.toString(),
+      })
+    },
+  })
+
 });
